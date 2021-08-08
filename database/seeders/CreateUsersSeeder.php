0baee3f117e5f24d351f769458396ee484b31c01
islamdiab-stack <islamdiab967@gmail.com>
2021-08-08 12:12:44 +0200

    Create seed for create users seeder

---
+++ database/seeders/CreateUsersSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class CreateUsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = [
+            [
+                'name' => 'Admin',
+                'email' => 'admin@test.com',
+                'is_admin' => '1',
+                'password' => Hash::make('123456789')
+            ],
+            [
+                'name' => 'user',
+                'email' => 'user@test.com',
+                'is_admin' => '0',
+                'password' => Hash::make('123456789')
+            ],
+        ];
+        foreach ($user as $key => $value) {
+            User::create($value);
+        }
+    }
+}

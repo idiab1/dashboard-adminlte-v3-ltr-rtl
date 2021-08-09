2a49fe8ecd9a34d09b8d32591ed71278f2ba477f
islamdiab-stack <islamdiab967@gmail.com>
2021-08-09 10:29:25 +0200

    Create new file for breadcrumb and replace breadcrumb-item to yield(breadcrumb) for change for each of page

---
+++ resources/views/admin/include/breadcrumb.blade.php
@@ -0,0 +1,13 @@
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>@yield('page_name')</h1>
+            </div>
+            <div class="col-sm-6">
+                @yield('breadcrumb')
+            </div>
+        </div>
+    </div><!-- /.container-fluid -->
+</section>

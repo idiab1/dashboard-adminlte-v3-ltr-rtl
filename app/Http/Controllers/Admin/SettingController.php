cc2a056e7140a34bc8f49bd0b2de15309a3d0096
islamdiab-stack <islamdiab967@gmail.com>
2021-08-09 15:37:43 +0200

    Type two methods like edit, update where edit for find setting by id then return edit page view, and update for validate on all data coming from users and update all data then redirect to admin home page

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -3,51 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Setting;
 use Illuminate\Http\Request;
 
 class SettingController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
 
     /**
      * Show the form for editing the specified resource.
@@ -57,7 +17,8 @@ class SettingController extends Controller
      */
     public function edit($id)
     {
-        //
+        $setting = Setting::find($id);
+        return view('admin.setting.edit', compact('setting'));
     }
 
     /**
@@ -69,17 +30,25 @@ class SettingController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
-    }
+        // validate on all data coming from users
+        $this->validate($request, [
+            'web_name' => 'string',
+            'phone_number',
+            'web_email' => 'email',
+            'address' => 'string'
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        ]);
+
+        $setting = Setting::find($id);
+
+        // Update
+        $setting->update([
+            'web_name' => $request->web_name,
+            'phone_number' => $request->phone_number,
+            'web_email' => $request->web_email,
+            'address' => $request->address
+        ]);
+
+        return redirect()->route('admin.home');
     }
 }

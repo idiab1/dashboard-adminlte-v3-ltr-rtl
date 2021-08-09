6fe55a0fa76adf0a3eec40579a0aa8fdbf3eb205
islamdiab-stack <islamdiab967@gmail.com>
2021-08-09 12:11:02 +0200

    Create new seeder for setting where SettingSeeder for create column in setting table

---
+++ database/seeders/SettingSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Setting;
+use Illuminate\Database\Seeder;
+
+class SettingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Setting::create([
+            'web_name' => 'Web Name',
+            'phone_number' => '+123456789',
+            'web_email' => 'web@app.com',
+            'address' => 'Egypt',
+        ]);
+    }
+}

ee120619e1b930e015e8b5a3f148662ee68ae49f
islamdiab-stack <islamdiab967@gmail.com>
2021-08-11 10:53:44 +0200

    Add image and name for user authantication in sidebar component

--- resources/views/admin/include/sidebar.blade.php
+++ resources/views/admin/include/sidebar.blade.php
@@ -11,10 +11,10 @@
         <!-- Sidebar user (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                {{-- <img src="" class="img-circle elevation-2" alt="User Image"> --}}
+                <img src="{{asset('uploads/users/avatar.png')}}" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">Alexander Pierce</a>
+                <a href="#" class="d-block">{{auth()->user()->name}}</a>
             </div>
         </div>
 

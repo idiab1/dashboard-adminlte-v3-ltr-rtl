cf9cc3990955b327b3a188a9e4771e1da8cb12d4
islamdiab-stack <islamdiab967@gmail.com>
2021-08-11 15:01:58 +0200

    Create new function for get avatar path attribute

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get avatar path attribute
+     */
+    public function getAvatarPathAttribute()
+    {
+        return asset('uploads/users/' . $this->avatar);
+    }
 }

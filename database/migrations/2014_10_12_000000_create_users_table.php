5ec01f8126e613f74eba3958ab623550db153ce7
islamdiab-stack <islamdiab967@gmail.com>
2021-08-12 13:11:37 +0200

    Remove avatar column from migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,6 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->boolean('is_admin')->nullable()->default(0);
             $table->string('password');
-            $table->string('avatar')->nullable()->default('default.png');
             $table->rememberToken();
             $table->timestamps();
         });
